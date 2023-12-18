71bb888a56c53a78fafcc89c34492f4c6aed26ee
EStherMmutisya <starjacob994@gmail.com>
2023-12-18 10:56:16 +0300

    added comments and made necessary changes to my code

--- challenge1.js
+++ challenge1.js
@@ -1,18 +1,26 @@
+//imporinng a module from node.js
 const readline = require('readline');
 
+//creating an interface that will read input from the user
 const rl = readline.createInterface({
   input: process.stdin,
   output: process.stdout
 });
 
+// a functiion to calculate the grade and marks of a student
 function gradeCalculator() {
+    //Getting iput from the user for marks
   rl.question('Enter the marks of the students (between 0 and 100): ', (userInput) => {
+    //converting the input into a number
     const marks = parseFloat(userInput);
 
+    //checking if the input is contained in the number bracket
     if (isNaN(marks) || marks < 0 || marks > 100) {
       console.log('Invalid input. Enter a number between 0 and 100.');
     } else {
       let grade;
+
+      // calculting the marks and grade
 
       if (marks > 79) {
         grade = 'A';
